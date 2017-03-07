eeb02bf81829c57681bfa32206311bf74e0bab21
Crymsius <romain.maurizot@gmail.com>
2017-03-07 16:23:57 +0100

    [feat] ajout des toggles intersection, changement des autres barrières, ajout des lignes de cases "néant" autour de la grille

--- Assets/Editor/LevelEditor.cs
+++ Assets/Editor/LevelEditor.cs
@@ -12,14 +12,15 @@ public class LevelEditor : EditorWindow {
     private int gridWidthPrivate;
     private int gridHeightPrivate;
     public string[,] gridContent = new string[0,0];
-    public string[,] gridLayoutVertical = new string[0,0];
-    public string[,] gridLayoutHorizontal = new string[0,0];
+    public bool[,] gridLayoutVertical = new bool[0,0];
+    public bool[,] gridLayoutHorizontal = new bool[0,0];
+    public bool[,] gridLayoutIntersection = new bool[0,0];
 
     public string gridString;
 
 
     [MenuItem ("My Tools/ Level Editor %g")]
-    private static void showEditor()
+    private static void showEditor ()
     {
         EditorWindow.GetWindow<LevelEditor> (false, "Grid Editor");
     }
@@ -33,102 +34,127 @@ public class LevelEditor : EditorWindow {
         gridHeight = EditorGUILayout.IntField ("Grid Height", gridHeight);
         gridString = EditorGUILayout.TextField ("Grid String", gridString);
         if (GUILayout.Button("Reload Grid")) {
-//            Debug.Log ("reload grid");
             gridContent = new string[gridHeight,gridWidth];
-            gridLayoutVertical = new string[gridHeight,gridWidth-1];
-            gridLayoutHorizontal = new string[gridHeight-1,gridWidth];
+            gridLayoutVertical = new bool[gridHeight,gridWidth-1];
+            gridLayoutHorizontal = new bool[gridHeight-1,gridWidth];
+            gridLayoutIntersection = new bool[gridHeight-1,gridWidth-1];
             gridHeightPrivate = gridHeight;
             gridWidthPrivate = gridWidth;
         }
 
         GUILayout.Label ("Grid customization", EditorStyles.boldLabel);
-        for (int i = 0; i < gridHeightPrivate; i++) {
-            EditorGUILayout.BeginHorizontal ();
-            for (int j = 0; j < gridWidthPrivate; j++) {
+        for (int i = 0; i < gridHeightPrivate; i++) 
+		{
+			/* DEBUT D'UNE LIGNE DE CASES */ 
+
+			EditorGUILayout.BeginHorizontal (GUILayout.MaxWidth(gridWidthPrivate));
+
+			for (int j = 0; j < gridWidthPrivate; j++) 
+			{
                 gridContent [i, j] = EditorGUILayout.TextField (gridContent[i, j], GUILayout.Width(40),GUILayout.Height(40));
-                GUILayout.Space (5);
-                if (j != gridWidthPrivate - 1) {
-                    gridLayoutVertical [i, j] = EditorGUILayout.TextField (gridLayoutVertical [i, j], GUILayout.Width (10), GUILayout.Height (40));
+                
+				if (j != gridWidthPrivate - 1) 
+				{
+					EditorGUILayout.BeginHorizontal (GUILayout.Width(25));
+					EditorGUILayout.BeginVertical (GUILayout.MaxHeight(40));
+					GUILayout.FlexibleSpace ();
+
+                    gridLayoutVertical [i, j] = EditorGUILayout.Toggle (gridLayoutVertical [i, j]);
+
+					GUILayout.FlexibleSpace ();
+					EditorGUILayout.EndVertical ();
+					EditorGUILayout.EndHorizontal ();
                 }
-                GUILayout.Space (5);
             }
             EditorGUILayout.EndHorizontal ();
-            EditorGUILayout.BeginHorizontal ();
-            for (int j = 0; j < gridWidthPrivate; j++) {
-                if (i != gridHeightPrivate -1) {
-                    gridLayoutHorizontal [i, j] = EditorGUILayout.TextField (gridLayoutHorizontal [i, j], GUILayout.Width (40), GUILayout.Height (10));
+			/* FIN D'UNE LIGNE DE CASES */ 
+
+			/* DEBUT D'UNE LIGNE DE BARRIERES */
+			EditorGUILayout.BeginHorizontal (GUILayout.MaxWidth(gridWidthPrivate ));
+            for (int j = 0; j < gridWidthPrivate; j++)
+			{
+                if (i != gridHeightPrivate -1)
+				{
+					GUILayout.Space (17);
+					gridLayoutHorizontal [i, j] = EditorGUILayout.Toggle (gridLayoutHorizontal [i, j]);
+					GUILayout.Space (17);
+
+					if (j != gridWidthPrivate - 1)
+					{
+						GUILayout.Space (5);
+						gridLayoutIntersection [i, j] = EditorGUILayout.Toggle (gridLayoutIntersection [i, j]);
+						GUILayout.Space (6);
+					}
                 }
-                GUILayout.Space (24);
             }
             EditorGUILayout.EndHorizontal ();
-        }
-        if (GUILayout.Button("Generate Grid")) {
-            gridString = this.generateGrid ();
-            //            Debug.Log (this.generateGrid());
         }
 
-//        groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
-//        myBool = EditorGUILayout.Toggle ("Toggle", myBool);
-//        myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
-//        EditorGUILayout.EndToggleGroup ();
+        if (GUILayout.Button("Generate Grid")) 
+		{
+            gridString = this.generateGrid ();
+        }
     }
 
-    public string generateGrid() {
+
+    public string generateGrid () {
+		//l : horizontal
+		//k : vertical
         //String vide au départ, va être populé par le reste afin de créer le résultat à rendre
         string resultString = "";
+
+		// en-tête
+		resultString = string.Concat(resultString, "N");
+		for (int l = 0; l < gridWidth; l++) {
+			resultString = string.Concat(resultString, "-N");
+		}
+		resultString = string.Concat(resultString, "-N+");
+
+		//pour chaque ligne
         for (int k = 0; k < gridHeight; k++) {
-            if (k != 0 && k != gridHeight) {
-                resultString = string.Concat(resultString, "+");
-            }
+			resultString = string.Concat(resultString, "N");
+          
+			//chaque case (= colone)
             for (int l = 0; l < gridWidth; l++) {
                 //ajoute le contenu de chaque case, dans l'ordre
-                if (l != 0 && l != gridWidth) {
-                    resultString = string.Concat (resultString, "-");
+                resultString = string.Concat (resultString, "-");
+
+                resultString = string.Concat (resultString, stringTranslator (gridContent[k,l]));
+				if (l != gridWidth -1 && gridLayoutVertical[k,l]) {
+					resultString = string.Concat (resultString, "D");
                 }
-                resultString = string.Concat(resultString, stringTranslator("gridContent",gridContent[k,l]));
-                if (l != gridWidth -1 ) {
-                    resultString = string.Concat(resultString, stringTranslator("gridLayoutVertical",gridLayoutVertical[k,l]));
-                }
-                if (k != gridHeight -1) {
-                    resultString = string.Concat(resultString, stringTranslator("gridLayoutHorizontal",gridLayoutHorizontal[k,l]));
-                }
-//                Debug.Log (gridContent[k,l]);
-//                Debug.Log (resultString);
+				if (k != gridHeight -1 && gridLayoutHorizontal[k,l]) {
+                    resultString = string.Concat (resultString, "B");
+				}
+				if (l != gridWidth -1 && k != gridHeight -1 && gridLayoutIntersection[k,l]) {
+					resultString = string.Concat (resultString, "I");
+				}
             }
+			resultString = string.Concat(resultString, "-N+");
+
         }
-//        Debug.Log (string.Concat(gridContent));
+		// fermeture
+		resultString = string.Concat(resultString, "N");
+		for (int l = 0; l < gridWidth; l++) {
+			resultString = string.Concat(resultString, "-N");
+		}
+		resultString = string.Concat(resultString, "-N");
+
         return resultString;
     }
-
-    public string stringTranslator(string caseType, string content) {
-        switch (caseType) {
-        case "gridContent":
-            switch (content) {
-            case "N":
-                return "N";
-            case "switch":
-                return "S";
-            default:
-                return "V";
-                break;
-            }
-            break;
-        case "gridLayoutHorizontal":
-            if (content!= null) {
-                return "B";
-            } else {
-                return "";
-            }
-            break;
-        case "gridLayoutVertical":
-            if (content!= null) {
-                return "D";
-            } else {
-                return "";
-            }
+		
+	/**
+	 * stringTranslator(stringcontent)
+	 * Renvoi une lettre en fonction du contenu
+	**/
+    public string stringTranslator(string content) {
+        switch (content) {
+        case "N":
+            return "N";
+        case "switch":
+            return "S";
         default:
-            Debug.Log ("error stringTranslator: caseType non valide");
-            return "error";
+            return "V";
             break;
         }
     }
