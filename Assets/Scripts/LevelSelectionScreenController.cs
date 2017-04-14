b23af7e74aa4e9d832984f2db8436ce33d0b493d
Crymsius <romain.m.mars@gmail.com>
2017-04-14 12:34:03 +0200

    [feat] first draft to level selection : can properly load gridScreen scene

---
+++ Assets/Scripts/LevelSelectionScreenController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionScreenController : MonoBehaviour {
+
+    public void StartQuickGame()
+    {
+        SceneManager.LoadScene("GridScreen");
+    }
+}
