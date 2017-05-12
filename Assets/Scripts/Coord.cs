5ab9ebc5680869270fd075779868a9486bc98574
Crymsius <romain.maurizot@gmail.com>
2017-05-12 15:25:07 +0200

    [feat]New operators for coord structure : ==, !=, Equals(), GetHashCode()

--- Assets/Scripts/Coord.cs
+++ Assets/Scripts/Coord.cs
@@ -18,7 +18,7 @@ public struct Coord {
     /// <param name="a"></param>
     /// <param name="b"></param>
     /// <returns> (xa + xb, ya + yb) </returns>
-    public static Coord operator+(Coord a, Coord b){
+    public static Coord operator +(Coord a, Coord b){
         return new Coord (a.x + b.x, a.y + b.y);
     }
 
@@ -28,7 +28,7 @@ public struct Coord {
     /// <param name="a"></param>
     /// <param name="b"></param>
     /// <returns> (xa - xb, ya - yb) </returns>
-    public static Coord operator-(Coord a, Coord b){
+    public static Coord operator -(Coord a, Coord b){
         return new Coord (a.x - b.x, a.y - b.y);
     }
 
@@ -38,8 +38,50 @@ public struct Coord {
     /// <param name="a"> int </param>
     /// <param name="b"> Coord </param>
     /// <returns> a * (xb, yb) </returns>
-    public static Coord operator*(int a, Coord b){
+    public static Coord operator *(int a, Coord b){
         return new Coord (a* b.x, a * b.y);
+    }
+
+    /// <summary>
+    /// Operator == for an int with coordinates
+    /// </summary>
+    /// <param name="a"> Coord </param>
+    /// <param name="b"> Coord </param>
+    /// <returns> true if a==b, else false </returns>
+    public static bool operator ==(Coord a, Coord b){
+        return a.x == b.x && a.y == b.y ? true: false;
+    }
+
+    /// <summary>
+    /// Operator != for an int with coordinates
+    /// </summary>
+    /// <param name="a"> Coord </param>
+    /// <param name="b"> Coord </param>
+    /// <returns> true if a!=b, else false </returns>
+    public static bool operator !=(Coord a, Coord b){
+        return a.x != b.x || a.y != b.y ? true: false;
+    }
+
+    /// <summary>
+    /// Operator Object.Equals ()
+    /// </summary>
+    /// <param name="o">Coord to compare</param>
+    /// <returns>true or false</returns>
+    public override bool Equals (object o) {
+        try {
+            return (bool) (this == (Coord) o);
+        } catch {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// GetHashCode method
+    /// </summary>
+    /// <returns>a unique key for a 2D vector</returns>
+    public override int GetHashCode () {
+        int tmp = ( y +  ((x+1)/2));
+        return x +  ( tmp * tmp);;
     }
 
     /// <summary>
