86a6002535f4e6ad53830e3a48f3d98f64614667
Crymsius <romain.maurizot@gmail.com>
2017-06-17 11:46:59 +0200

    [fix]grid screen stuck on loading (missing files)

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -29,7 +29,7 @@ public class GameHandler : MonoBehaviour {
         isOver = false;
         activePlayer = 0;
 
-        typePlayer = new List<string>() { "IA", "human" }; // Test, à remplacer par un appel au lancement d'une partie
+        typePlayer = new List<string>() { "human", "human" }; // Test, à remplacer par un appel au lancement d'une partie
         GameObject.Find("IAHandler").GetComponent<IAMain>().typePlayers = typePlayer;
 
         /// [switchVar]
