da9fe5ce2f4778de744f4ada963d4670ba156125
LAPTOP-6Q7C8FBA\Reaskel <lextrait.bastien@gmail.com>
2017-07-04 19:59:51 +0200

    [perf] improved IA decision speed

--- Assets/Scripts/DecisionTreeNode.cs
+++ Assets/Scripts/DecisionTreeNode.cs
@@ -37,46 +37,55 @@ public class DecisionTreeNode {
 		variant = VariantSelector.variant;
     }
 
+	public IEnumerator AssignDeployment(){
+		yield return new WaitForSeconds (0f);
+		DeploymentTree ();
+	}
     public void DeploymentTree() { // Fonction récursive mettant à jour l'arbre de décision.
-        if (depth == depthMax)
+		if (depth == depthMax)
             /*do nothing*/
             ;
+		else if (children.Count == 0 && !position.isVictory) { // On ne considère la suite que si la position actuelle n'est pas une position de victoire
+			List<Coord> playables = new List<Coord> (); // enregistrement de tous les coups jouables
+			for (int i = 0; i < position.hDim; i++)
+				for (int j = 0; j < position.vDim; j++)
+					if (isPlayable (new Coord (i, j), gravity))
+						playables.Add (new Coord (i, j));
+			
+			bool existsVictory = false;
 
-        else if (children.Count == 0 && !position.isVictory) // On ne considère la suite que si la position actuelle n'est pas une position de victoire
-        {
-            List<Coord> playables = new List<Coord>(); // enregistrement de tous les coups jouables
-            for (int i = 0; i < position.hDim; i++)
-                for (int j = 0; j < position.vDim; j++)
-                    if (isPlayable(new Coord(i, j), gravity))
-                        playables.Add(new Coord(i, j));
-
-            bool existsVictory = false;
-
-            foreach (Coord play in playables)
-            { // Création des nouveaux noeuds fils
-                // On commence par effectuer le play et vérifier si des triggers sont traversés.
-				children.Add(play, new DecisionTreeNode(1 - player, depth + 1, depthMax, new Matrix(position, myAtlas), myAtlas, 
-					gravity, 0, new Dictionary<Coord, DecisionTreeNode>(), typePlayers));
+			foreach (Coord play in playables) { // Création des nouveaux noeuds fils
+				// On commence par effectuer le play et vérifier si des triggers sont traversés.
+				children.Add (play, new DecisionTreeNode (1 - player, depth + 1, depthMax, new Matrix (position, myAtlas), myAtlas, 
+					gravity, 0, new Dictionary<Coord, DecisionTreeNode> (), typePlayers));
 				if (variant == 0)
-					PlayThenTriggered_BastienVersion (play); 
-				else TriggeredUntilPlayed_RomainVersion (play);
+					PlayThenTriggered_BastienVersion (play);
+				else
+					TriggeredUntilPlayed_RomainVersion (play);
 
-                //Puis on attribue un score au plateau. Si une puissance 4 est repérée, on la signale.
-                children[play].score = children[play].position.MeasureScore(children[play].player);
-                if (children[play].position.isVictory)
-                    existsVictory = true;
-            }
+				//Puis on attribue un score au plateau. Si une puissance 4 est repérée, on la signale.
+				children [play].score = children [play].position.MeasureScore (children [play].player);
+				if (children [play].position.isVictory)
+					existsVictory = true;
+			}
 
-            foreach (Coord play in playables) {
-                if (!existsVictory || (existsVictory && children[play].position.isVictory))
-                    children[play].DeploymentTree();
-                else
-                    children[play].considered = false;
-            }
-        }
-
-        else foreach (Coord key in children.Keys) if (children[key].considered)
-                    children[key].DeploymentTree();
+			foreach (Coord play in playables) {
+				if (!existsVictory || (existsVictory && children [play].position.isVictory)) {
+					if (depth == 0)
+						GameObject.Find ("IAHandler").GetComponent<IAMain> ().StartCoroutine (children [play].AssignDeployment ());
+					else
+						children [play].DeploymentTree ();
+				} else
+					children [play].considered = false;
+			}
+		} else
+			foreach (Coord key in children.Keys)
+				if (children [key].considered) {
+					if (depth == 0)
+						GameObject.Find ("IAHandler").GetComponent<IAMain> ().StartCoroutine (children [key].AssignDeployment ());
+					else
+						children [key].DeploymentTree ();
+				}
     }
 	public void PlayThenTriggered_BastienVersion(Coord play){
 		children[play].position.values[play] = 1 - player;
